9e5d714f5466dfad38c03f5db18a27d055804e4f
GabrieldsReis <147285271+GabrieldsReis@users.noreply.github.com>
2024-03-26 20:14:04 -0300

    Add files via upload

---
+++ ex 7/ex7.js
@@ -0,0 +1,76 @@
+ 
+
+
+ if ( sessionStorage.getItem ("nome" ) == null ) {
+ window.location.href = "D:/xampp/htdocs/Gabriel%20Reis/Hamburgueria/login/login.html"
+  
+
+ }
+
+
+
+
+var x = document.getElementsByClassName("box-container")[0].clientHeight;
+
+document.getElementsByClassName("box-price")[0].style.height = x + "px";
+
+
+function mudandoQnt(operacao, elemento) {
+
+
+
+   var a = elemento.getElementsByTagName("span")[0].innerHTML * 1;
+
+   var conta = 0;
+
+   if (operacao == "+") {
+      conta = a + 1;
+      elemento.getElementsByTagName("span")[0].innerHTML = conta;
+
+   }
+
+   if (operacao == "-") {
+   conta = a - 1;
+      elemento.getElementsByTagName("span")[0].innerHTML = conta;
+   
+
+   
+
+   }
+
+
+  
+
+   
+
+elemento.getElementsByTagName("span")[1].innerHTML = conta;
+
+elemento.getElementsByTagName("span")[3].innerHTML = ((conta*elemento.getElementsByTagName("span")[2].innerHTML.replace( "," , ".")).toFixed(2)*1 +"").replace ( "." ,",");
+
+
+
+
+var precos =  document.getElementsByClassName("precos");
+
+
+var total = 0; 
+
+
+for (let i = 0; i < precos.length;i++){
+   
+   
+   total = total + precos[i].innerHTML.replaceAll ("," , ".")*1;
+
+   
+   
+
+}
+/*
+total = precos[0].innerHTML.replaceAll(",", "." )*1 + precos[1].innerHTML.replaceAll("," , ".")*1 + precos[2].innerHTML.replaceAll("," , ".")*1;
+modo ruim
+*/
+
+document.getElementById("Total").innerHTML = (total.toFixed(2)*1 + "").replace("." , ",");
+      
+
+}
