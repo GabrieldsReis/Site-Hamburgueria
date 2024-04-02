937afc1ea45191d783366fdf9b40d1aa8d95567e
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-04-02 20:23:32 -0300

    Add files via upload

---
+++ Hamburgueria/cadastro/cadastro.js
@@ -0,0 +1,57 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+
+import {child, get, getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+window.cadastro = function cadastro() {
+
+
+var usuario = document.getElementById ("nome").value;
+
+var senha = document.getElementById ("senha").value;
+
+if (usuario == "" || senha == "") {
+
+document.getElementById ("mudar").innerHTML = "preencha os campos" 
+}
+
+
+
+else {
+
+   
+
+ get(child(ref(database), 'usuarios/' + usuario)).then
+            ((snapshot) => {
+
+                if (snapshot.exists()){
+              
+document.getElementById("mudar").innerHTML = "Usuario ja registrado"
+                   
+             } else {
+
+                document.getElementById("erro").innerHTML = "usuario não cadastrado";
+
+             }
+            });
+
+
+set(ref (database, 'usuarios/' + usuario), 
+
+{
+
+senha: senha
+
+}
+
+);
+
+}
+
+
+
+
+}
