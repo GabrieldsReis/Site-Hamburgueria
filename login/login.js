e690f78d0d859e86636d5c2a1dbb40b73a035c67
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-04-16 19:49:33 -0300

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -3,6 +3,19 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebas
 import {child, get, getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBJodswmt7lrtvmvTlCG1sLJa9bj0FdIs0",
+    authDomain: "herogeek-9cf88.firebaseapp.com",
+    databaseURL: "https://herogeek-9cf88-default-rtdb.firebaseio.com",
+    projectId: "herogeek-9cf88",
+    storageBucket: "herogeek-9cf88.appspot.com",
+    messagingSenderId: "633995872369",
+    appId: "1:633995872369:web:45c6b71869455dd52bb3d0"
+
+
+
+};
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
@@ -13,10 +26,12 @@ window.login = function login() {
 
     var senha = document.getElementById("senha").value;
 
-
+console.log("ok")
     get(child(ref(database), 'usuarios/' + usuario)).then
     ((snapshot) => {
 
+
+       
         if (snapshot.exists()){
             console.log(snapshot.val().senha);
             console.log(snapshot.val().senha == senha)
@@ -24,7 +39,7 @@ window.login = function login() {
 
             if (snapshot.val().senha== senha){
                
-                sessionStorage.setItem("nome")
+                sessionStorage.setItem("nome", "Logado" );
                 window.location.href = "../ex%207/ex7.html"
 
             }
