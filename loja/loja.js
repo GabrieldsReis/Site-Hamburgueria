ea1944dd6c38fed5c3ec3c6deb9859324a2cec3b
Gabriel Reis <147285271+GabrieldsReis@users.noreply.github.com>
2024-05-30 21:46:42 -0300

    Add files via upload

--- loja/loja.js
+++ loja/loja.js
@@ -1,14 +1,9 @@
- 
+
+var x = document.getElementsByClassName("mx-2")[0].clientHeight;
+
+document.getElementsByClassName("mx-2")[0].style.height = x + "px";
 
 
-
-
-
-
-
-var x = document.getElementsByClassName("box-container")[0].clientHeight;
-
-document.getElementsByClassName("box-price")[0].style.height = x + "px";
 
 
 function mudandoQnt(operacao, elemento) {
@@ -26,47 +21,42 @@ function mudandoQnt(operacao, elemento) {
    }
 
    if (operacao == "-") {
-   conta = a - 1;
+      conta = a - 1;
       elemento.getElementsByTagName("span")[0].innerHTML = conta;
-   
 
-   
+
+
 
    }
 
+   elemento.getElementsByTagName("span")[1].innerHTML = conta;
 
-  
-
-   
-
-elemento.getElementsByTagName("span")[1].innerHTML = conta;
-
-elemento.getElementsByTagName("span")[3].innerHTML = ((conta*elemento.getElementsByTagName("span")[2].innerHTML.replace( "," , ".")).toFixed(2)*1 +"").replace ( "." ,",");
+   elemento.getElementsByTagName("span")[3].innerHTML = ((conta * elemento.getElementsByTagName("span")[2].innerHTML.replace(",", ".")).toFixed(2) * 1 + "").replace(".", ",");
 
 
 
 
-var precos =  document.getElementsByClassName("precos");
+   var precos = document.getElementsByClassName("precos");
 
 
-var total = 0; 
+   var total = 0;
 
 
-for (let i = 0; i < precos.length;i++){
-   
-   
-   total = total + precos[i].innerHTML.replaceAll ("," , ".")*1;
+   for (let i = 0; i < precos.length; i++) {
 
-   
-   
 
-}
-/*
-total = precos[0].innerHTML.replaceAll(",", "." )*1 + precos[1].innerHTML.replaceAll("," , ".")*1 + precos[2].innerHTML.replaceAll("," , ".")*1;
-modo ruim
-*/
+      total = total + precos[i].innerHTML.replaceAll(",", ".") * 1;
 
-document.getElementById("Total").innerHTML = (total.toFixed(2)*1 + "").replace("." , ",");
-      
+
+
+
+   }
+   /*
+   total = precos[0].innerHTML.replaceAll(",", "." )*1 + precos[1].innerHTML.replaceAll("," , ".")*1 + precos[2].innerHTML.replaceAll("," , ".")*1;
+   modo ruim
+   */
+
+   document.getElementById("Total").innerHTML = (total.toFixed(2) * 1 + "").replace(".", ",");
+
 
 }
